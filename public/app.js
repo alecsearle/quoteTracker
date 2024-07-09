667d8e5682483961956df79d22bf903a87d105c4
alecsearle <searle.alec@gmail.com>
2024-07-09 12:49:02 -0600

    quote page

--- public/app.js
+++ public/app.js
@@ -2,7 +2,7 @@ const URL = "http://localhost:8080";
 Vue.createApp({
   data() {
     return {
-      currentPage: "loading",
+      currentPage: "findQuote",
       user: {
         name: "",
         email: "",
